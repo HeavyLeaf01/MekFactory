9627625822a39e322d06744baaf15059a685f869
HeavyLeaf <gabigoolgabriell@gmail.com>
2026-03-31 13:11:43 -0300

    Update

--- mek-factory/Scripts/tela_menu_1.gd
+++ mek-factory/Scripts/tela_menu_1.gd
@@ -1,20 +1,16 @@
 extends Control
 
 func _ready():
-	# Garante que o menu comece escondido
+	
 	hide()
 
 func _input(event):
-	if event.is_action_pressed("abrir_menu"):
+	if event.is_action_pressed("Acao"):
 		toggle_menu()
 
 func toggle_menu():
-	visible = !visible # Inverte a visibilidade
+	visible = !visible
 	
-	# Opcional: Pausar o jogo enquanto o menu está aberto
-	get_tree().paused = visible
-	
-	# Liberar ou prender o mouse
 	if visible:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	else:
